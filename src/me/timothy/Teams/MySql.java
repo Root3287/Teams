d782b7941902dd6489eb77c588786b3b650dc578
Timothy Gibbons <tjgibbons@rootmc.com>
2015-05-25 15:51:51 -0500

    Database

--- src/me/timothy/Teams/MySql.java
+++ src/me/timothy/Teams/MySql.java
@@ -1,22 +1,88 @@
 package me.timothy.Teams;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class MySql {
-	private Teams instance;
-	public Connection conn;
-	private String host, port, user, password, db;
+	private Connection conn;
+	private String host, port, user, password, db, prefix;
+	private ResultSet result;
+	private int count;
+	private boolean errors;
+	//private static MySql instance = new MySql();
 
-	
-	public MySql(String host, String user, String Password, String DB, Teams p){
+	public MySql(){
+		
+	}
+	public MySql(String host, String user, String Password, String DB, String prefix){
 		this.host = host;
 		this.user = user;
 		this.password = Password;
 		this.db = DB;
-		instance = p;
+		this.prefix = prefix;
 	}
 	
 	public void connect(){
-		
+		try{
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection("jdbc://"+this.host+":"+this.port+"/"+this.db+"?user="+this.user+"&password="+this.password);
+		}catch(ClassNotFoundException e){
+			e.printStackTrace();
+		}catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public MySql query(String q){
+		try{
+			PreparedStatement ps = conn.prepareStatement(q);
+			ps.executeQuery();
+			this.result = ps.getResultSet();
+			this.count = ps.getMaxRows();
+			this.errors = false;
+		}catch(SQLException e){
+			e.printStackTrace();
+			this.errors = true;
+		}
+		return this;
+	}
+	
+	public MySql query(String q, String[] fields) throws SQLException{
+		try{
+			PreparedStatement ps = conn.prepareStatement(q);
+			
+			if(fields.length>0){
+				int i = 1;
+				for(String field : fields){
+					ps.setString(i, field);
+					i++;
+				}
+			}
+			ps.executeQuery();
+			this.result = ps.getResultSet();
+			this.count = ps.getMaxRows();
+			this.errors = false;
+		}catch(SQLException e){
+			e.printStackTrace();
+			this.errors = true;
+		}
+		return this;
+	}
+
+	public void create(String Table, String fields) throws SQLException{
+		query("CREATE TABLE "+Table+"("+fields+")");
+	}
+	
+	public ResultSet result(){
+		return this.result;
+	}
+	public boolean errors(){
+		return this.errors;
+	}
+	public int count(){
+		return this.count;
 	}
 }
--- src/me/timothy/Teams/Teams.java
+++ src/me/timothy/Teams/Teams.java
@@ -1,5 +1,7 @@
 package me.timothy.Teams;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.logging.Logger;
 
 import me.timothy.Teams.commands.ListTeams;
@@ -12,12 +14,28 @@ public class Teams extends JavaPlugin{
 	
 	public static Teams instance;
 	
+	public SettingsManager sm = SettingsManager.getInstance();
+	
 	public final Logger log = Logger.getLogger("Minecraft");
+	public MySql sql = new MySql();
+	
 	
 	public void onEnable() {
 		PluginManager pm = Bukkit.getServer().getPluginManager();
 		
 		getCommand("lt").setExecutor(new ListTeams(this));
+		
+		ResultSet query = sql.query("SELECT 1 FROM settings WHERE id<>1").result();
+		try {
+			if(query.next()){
+				
+			}else{
+				//create table
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
